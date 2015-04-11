8f41cc35fc0b2a7a25d61012df322895b6462956
James Wang <jameswang9909@hotmail.com>
2015-04-11 13:49:16 -0400

    Fix and solve Hashkill

--- Hashkill/Solution.py
+++ Hashkill/Solution.py
@@ -1,14 +1,14 @@
-import md5
+import hashlib, system
 
 encrypted_flag = "76370c8b218eacfdf6a33bd4c311575a"
-boroughs = ["Queens", "Manhatten", "Staten Island", "Bronx", "Brooklyn"]
+boroughs = ["queens", "manhattan", "staten island", "bronx", "brooklyn"]
 
 for firstNums in range(0,1000):
     for borough in boroughs:
         for lastNums in range(10000, 15000):
-            flag = "stuyctf{%s_%s_%s}" % (firstNums, borough, lastNums)
-            print flag
-            testHash = md5.new(flag)
-            if (testHash.digest() == encrypted_flag):
-                print flag
+            testing = "stuyctf{%s_%s_%s}" % (firstNums, borough, lastNums)
+            print testing
+            testHash = hashlib.md5(testing)
+            if (testHash.hexdigest() == encrypted_flag):
+                print "The flag is " + testing
                 sys.exit(0)
