cada9ade27f1fcb0959497af66f48b8a6e9cd345
James Wang <jameswang9909@hotmail.com>
2015-04-12 19:10:53 -0400

    Slight fixes in damage calculator, still doesn't work though

--- Benny The Bulbasaur/Solution.py
+++ Benny The Bulbasaur/Solution.py
@@ -13,23 +13,10 @@ def typeEffectiveness(moveType):
         return .5
     elif (doubleNotRly.find(moveType) != -1): # Really really not effective...
         return .25
-    elif (moveType == "Poison"):
+    elif (moveType == "Poison"): # Doesn't effect...
         return 0
     else: # Normal
         return 1
-
-def damageFormula(level, isSpecial, myAttack, enemyDefense, base, myType, enemyType, moveType, accuracy):
-    if (isSpecial):
-        myAttack = bulbasaur["SPATTACK"]
-        enemyDefense = scizor["SPDEFENSE"]
-        damage = (((2 * level + 10)/250.0) * (myAttack/enemyDefense) * base + 2)
-        damage *= modifier(myType, enemyType, moveType)
-        damage *= accuracy
-    else:
-        damage = (((2 * level + 10)/250.0) * (myAttack/enemyDefense) * base + 2)
-        damage *= modifier(myType, enemyType, moveType)
-        damage *= accuracy
-    return damage
 
 def modifier(myType, enemyType, moveType):
     if (myType.find(moveType) != -1): # STAB
@@ -40,30 +27,40 @@ def modifier(myType, enemyType, moveType):
     print "modifier is %s" % amount
     return amount
 
+def damageFormula(level, isSpecial, myAttack, enemyDefense, base, myType, enemyType, moveType, accuracy):
+    accuracy /= 100
+    # Takes into account special moves
+    if (isSpecial):
+        myAttack = bulbasaur["SPATTACK"]
+        enemyDefense = scizor["SPDEFENSE"]
+        damage = (((2 * level + 10)/250.0) * (myAttack/enemyDefense) * base + 2)
+        damage *= modifier(myType, enemyType, moveType)
+    else:
+        damage = (((2 * level + 10)/250.0) * (myAttack/enemyDefense) * base + 2)
+        damage *= modifier(myType, enemyType, moveType)
+    return damage
+
+# init
 maxDamage = [0, 0, 0, 0]
 isSpecial = False
-moveWithMostDamage = ["", "", "", ""]
 for move in range(0, len(moves)):
     if (moves[move][2] == "Special"): # Special attacks
         isSpecial = True
     else:
         isSpecial = False
     currDamage = damageFormula(100, isSpecial, bulbasaur["ATTACK"], scizor["DEFENSE"], moves[move][3], bulbasaur["TYPE"], scizor["TYPE"], moves[move][1], moves[move][4])
+    # currDamage = round(currDamage)
     print currDamage
     # Update max damage
     if (currDamage >= maxDamage[0]):
         maxDamage[0] = currDamage
-        moveWithMostDamage[0] = moves[move]
     elif (currDamage >= maxDamage[1]):
         maxDamage[1] = currDamage
-        moveWithMostDamage[1] = moves[move]
     elif (currDamage >= maxDamage[2]):
         maxDamage[2] = currDamage
-        moveWithMostDamage[2] = moves[move]
     elif (currDamage >= maxDamage[3]):
         maxDamage[3] = currDamage
-        moveWithMostDamage[3] = moves[move]
     print moves[move]
 
-print str(maxDamage) + " " + str(moveWithMostDamage)
-print maxDamage[0] + maxDamage[1] + maxDamage[2] + maxDamage[3]
+print str(maxDamage)
+print round(maxDamage[0] + maxDamage[1] + maxDamage[2] + maxDamage[3])
