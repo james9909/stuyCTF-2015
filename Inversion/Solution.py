5dcaf68112c13b145e498f1e709ab6f42bee3b68
James Wang <jameswang9909@hotmail.com>
2015-04-14 23:14:32 -0400

    Fix python script and add decoded images, so close to solving

--- Inversion/Solution.py
+++ Inversion/Solution.py
@@ -1,47 +1,31 @@
+# Basically the same as the encryption method so basically copy pasted
+
 import sys
 import math
 import struct
 
-p = lambda x: struct.pack('f', x)
-u = lambda x: struct.unpack('b', x)[0]
+p = lambda x: struct.pack('b', x)
+u = lambda x: struct.unpack('f', x)[0]
 
 if len(sys.argv) != 5:
-    sys.exit(1)
+	sys.exit(1)
 
 filename = sys.argv[1]
 r = float(sys.argv[2])
 keyx = int(sys.argv[3])
 keyy = int(sys.argv[4])
 
-enc = open(filename, 'rb')
+# Open and read encrypted file
+enc = open(filename, 'rb').read()
+decrypted = open(filename + str(r) + ".jpg", 'wb') # mlg names
 
-lookup = {}
-for k in xrange(256):
-    for l in xrange(256):
-        x = struct.unpack('b', chr(k))[0]
-        y = struct.unpack('b', chr(l))[0]
-
+# 8 because 8 bytes for each x and y
+for i in range(0,len(enc),8):
+        # Acquire the encrypted x and y values to decrypt
+	(x, y) = u(enc[i:i+4]), u(enc[i+4:i+8])
         try:
-            x1 = p(((r ** 2) * (x - keyx)) / ((x - keyx)** 2 + (y - keyy) ** 2))
-            x2 = p(((r ** 2) * (y - keyy)) / ((x - keyx)** 2 + (y - keyy) ** 2))
-            x3 = struct.unpack('<L', x1)[0]
-            x4 = struct.unpack('<L', x2)[0]
-
-            lookup[ (x3<<32)+x4 ] = (k, l)
-
+            # wolfram alpha inversion formula
+            decrypted.write(p(round(((r ** 2) * (x - keyx)) / ((x - keyx)** 2 + (y - keyy) ** 2))) \
+                    + p(round(((r ** 2) * (y - keyy)) / ((x - keyx)** 2 + (y - keyy) ** 2))))
         except ZeroDivisionError:
-            print p(float(x)) + p(float(y))
-
-
-going = 1
-decoded = ""
-while going:
-    try:
-        f1 = struct.unpack('<L', enc.read(4))[0]
-        f2 = struct.unpack('<L', enc.read(4))[0]
-        (x, y) = lookup[ (f1<<32)+f2 ]
-        decoded += chr(x)
-        decoded += chr(y)
-    except:
-        going = 0
-        print decoded
+            decrypted.write(p(round(x)) + p(round(y)))
--- Inversion/Solution.sh
+++ Inversion/Solution.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 for i in $(seq 10 99); do
-    for j in $(seq 0 360); do
-        for k in $(seq 0 360); do
-            python Solution.py image.jpg.enc $i $j $k
-        done
-    done
+    python Solution.py image.jpg.enc $i 0 0
 done
